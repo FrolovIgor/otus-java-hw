71d134145681fe79a7e5b2d43ae1d670aac7b280
iifrol10 <iifrol10@mts.ru>
2023-12-07 20:41:47 +0300

    [hw04-collections] add collections homework

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 rootProject.name = "otus-java-hw"
 include("hw01-gradle")
+include("hw04-collections")
 
 pluginManagement {
     val dependencyManagement: String by settings
@@ -14,3 +15,4 @@ pluginManagement {
         id("com.diffplug.spotless") version spotless
     }
 }
+include("hw04-collections")
