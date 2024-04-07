1fbada26ee06725c1b6610979026b74fec43da4d
iifrol10 <iifrol10@mts.ru>
2024-04-07 22:36:32 +0300

    [hw10-aop-logging] add generics to ioc

--- hw10-aop-logging/src/main/java/ru/otus/logging/Ioc.java
+++ hw10-aop-logging/src/main/java/ru/otus/logging/Ioc.java
@@ -6,33 +6,30 @@ import java.lang.reflect.Proxy;
 import java.util.Arrays;
 
 import ru.otus.logging.annotations.Log;
-import ru.otus.logging.service.TestLoggingInterface;
-import ru.otus.logging.service.impl.TestLogging;
 
 class Ioc {
     private Ioc() {
     }
 
-    static TestLoggingInterface createMyClass() {
-        InvocationHandler handler = new DemoInvocationHandler(new TestLogging());
-        return (TestLoggingInterface)
-                Proxy.newProxyInstance(Ioc.class.getClassLoader(), new Class<?>[]{TestLoggingInterface.class}, handler);
+    static<I,E extends I> I createMyClass(Class<I> interfaceType, E object) {
+        InvocationHandler handler = new LoggerInvocationHandler<I>(object);
+        return (I) Proxy.newProxyInstance(Ioc.class.getClassLoader(), new Class<?>[]{interfaceType}, handler);
     }
 
-    static class DemoInvocationHandler implements InvocationHandler {
-        private final TestLoggingInterface myClass;
+    static class LoggerInvocationHandler<T> implements InvocationHandler {
+        private final T proxyTarget;
 
-        DemoInvocationHandler(TestLogging myClass) {
-            this.myClass = myClass;
+        LoggerInvocationHandler(T proxyTarget) {
+            this.proxyTarget = proxyTarget;
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
-            if (myClass.getClass().getMethod(method.getName(), method.getParameterTypes()).isAnnotationPresent(Log.class)) {
+            if (proxyTarget.getClass().getMethod(method.getName(), method.getParameterTypes()).isAnnotationPresent(Log.class)) {
                 System.out.printf("executed method: %s, param: %s%n", method.getName(), Arrays.stream(args).toList());
             }
-            return method.invoke(myClass, args);
+            return method.invoke(proxyTarget, args);
         }
     }
 }
